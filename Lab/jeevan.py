50bec5ce2a3d41ceab4f79d97f0ea5134f71dabf
Jeevan Koshy <34179846+jee14k@users.noreply.github.com>
2019-10-24 20:13:25 +0530

    Add files via upload

---
+++ Lab/jeevan.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+
+# In[5]:
+
+def funct1(a,b):
+    print("Value of a: ",a)
+    print("Value of b: ",b)
+
+
+# In[6]:
+
+funct1(5,3)
+
+
+# In[11]:
+
+def funct2(c,d):
+    e=c*d
+    print("The product of is ",e)
+
+
+# In[12]:
+
+funct2(5,2)
+
+
+# In[ ]:
+
+
+
